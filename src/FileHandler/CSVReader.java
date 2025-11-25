0f252da81aa6555d61a5dfa5c0053fdd0bd74c11
Bishobos <mps.kajkage@gmail.com>
2025-11-25 09:52:43 +0100

    Changed CSVReader to single class and deleted interface. Added constructor and filename forrmatter to compensate.

--- src/FileHandler/CSVReader.java
+++ src/FileHandler/CSVReader.java
@@ -2,6 +2,16 @@ package FileHandler;
 
 import java.util.ArrayList;
 
-public interface CSVReader {
-    public ArrayList<String[]> read();
+public class CSVReader {
+    String filename;
+
+    public CSVReader(String filename){this.filename = formatFilename(filename);}
+
+    public ArrayList<String[]> read() {
+        return LineSplitter.lineSplitter(DecimalFormatting.commaToDot(Read.read(filename)));
+    }
+
+    private String formatFilename(String filename){
+        return "src/Data/" + filename + ".csv";
+    }
 }
