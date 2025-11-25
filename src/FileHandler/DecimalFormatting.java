2a56b52f406734fd3ef45b843340b56f75c337c1
Bishobos <mps.kajkage@gmail.com>
2025-11-25 09:35:18 +0100

    TransactionReader be afloat

---
+++ src/FileHandler/DecimalFormatting.java
@@ -0,0 +1,22 @@
+package FileHandler;
+
+import java.util.ArrayList;
+
+class DecimalFormatting {
+    static ArrayList<String> commaToDot(ArrayList<String> toFormat){
+        ArrayList<String> returnString = new ArrayList<>();
+        for(String string: toFormat){
+            String formattedString = string.replace(",", ".");
+            returnString.add(formattedString);
+        }
+        return returnString;
+    }
+    static ArrayList<String> dotToComma(ArrayList<String> toFormat){
+        ArrayList<String> returnString = new ArrayList<>();
+        for(String string: toFormat){
+            String formattedString = string.replace(".", ",");
+            returnString.add(formattedString);
+        }
+        return returnString;
+    }
+}
