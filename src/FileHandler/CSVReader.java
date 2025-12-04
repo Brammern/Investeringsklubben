0f3434a84db68a2261fed666bf5f6d02f8573eb7
Bishobos <mps.kajkage@gmail.com>
2025-12-04 10:12:48 +0100

    added Docs

--- src/FileHandler/CSVReader.java
+++ src/FileHandler/CSVReader.java
@@ -3,16 +3,29 @@ package FileHandler;
 import java.util.ArrayList;
 
 /**
- *
+ * CSVReader is a Class used to get data from CSV files and format the data appropriately
  * Has Attributes: <br>
- *
+ *{@link #filename} <br>
  * Has Methods: <br>
+ * {@link #CSVReader(String)} <br>
+ * {@link #read()}<br>
  */
 public class CSVReader {
+    /**
+     * name of the file to read from
+     */
     String filename;
 
+    /**
+     * constructor, gets and formats a filename using {@link #formatFilename(String)} then sets {@link #filename}
+     * @param filename String of the file to read from
+     */
     public CSVReader(String filename){this.filename = formatFilename(filename);}
 
+    /**
+     * reads csv file {@link #filename} using {@link Read} then formats it using {@link DecimalFormatting} and {@link LineSplitter}
+     * @return ArrayList&ltString[]&gt containing data read from file
+     */
     public ArrayList<String[]> read() {
         return LineSplitter.lineSplitter(DecimalFormatting.commaToDot(Read.read(filename)));
     }
