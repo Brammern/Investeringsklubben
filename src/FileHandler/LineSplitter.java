b9e18506663de23918a0d0c2196a852c3fbbccd1
Bishobos <mps.kajkage@gmail.com>
2025-12-04 10:17:27 +0100

    added Docs

--- src/FileHandler/LineSplitter.java
+++ src/FileHandler/LineSplitter.java
@@ -3,12 +3,16 @@ package FileHandler;
 import java.util.ArrayList;
 
 /**
- *
- * Has Attributes: <br>
- *
+ * LineSplitter is used to split a String into a String array using the regex ';'
  * Has Methods: <br>
+ * {@link #lineSplitter(ArrayList)}
  */
 class LineSplitter {
+    /**
+     * lineSplitter is a static method that runs through a ArrayList&ltString&gt splitting all Strings into String arrays using regex ';'
+     * @param toFormat ArrayList&ltString&gt to format
+     * @return ArrayList&ltString[]&gt of formatted data
+     */
     static ArrayList<String[]> lineSplitter(ArrayList<String> toFormat){
         ArrayList<String[]> formatted = new ArrayList<>();
         for (String line: toFormat){
